f5459e1f22f552bdde647794243d90d4a19d9f83
MarkTyu <26tyurim@mbbc.qld.edu.au>
2024-02-09 14:28:32 +1000

    added zork

---
+++ Objects/Zork.py
@@ -0,0 +1,20 @@
+from GameFrame import RoomObject
+import random
+
+class Zork(RoomObject):
+    """
+    A class for the game's antagoist
+    """
+    def __init__(self, room, x, y):
+        """
+        Initialise the Boss object
+        """
+        # include attributes and methods from RoomObject
+        RoomObject.__init__(self, room, x, y)
+        
+        # set image
+        image = self.load_image("Zork.png")
+        self.set_image(image,135,165)
+
+        # set inital movement
+        self.y_speed = random.choice([-10,10])
